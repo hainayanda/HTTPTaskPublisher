537065caa79592892b95e570174fbb1ee0a631d8
Nayanda Haberty <hainayanda@outlook.com>
2023-06-15 18:40:19 +0700

    Add Extensions

---
+++ HTTPTaskPublisher/Classes/Extensions/Publisher+Extensions.swift
@@ -0,0 +1,41 @@
+//
+//  Publisher+Extensions.swift
+//  HTTPTaskPublisher
+//
+//  Created by Nayanda Haberty on 26/5/23.
+//
+
+import Foundation
+import Combine
+
+extension Publisher where Output == (data: Data, response: URLResponse), Failure == URLError {
+    
+    func httpResponseOnly() -> AnyPublisher<URLSession.HTTPDataTaskPublisher.Response, HTTPURLError> {
+        tryMap { output in
+            guard let response = output.response as? HTTPURLResponse else {
+                throw HTTPURLError.expectHTTPResponse(data: output.data, response: output.response)
+            }
+            return (output.data, response)
+        }
+        .mapErrorToHTTPURLError()
+        .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher where Output == URLSession.HTTPDataTaskPublisher.Output, Failure == HTTPURLError {
+    
+    public typealias DecodedOutput<Decoded> = (decoded: Decoded, response: HTTPURLResponse)
+    
+    public func decode<Decoded: Decodable, Decoder: TopLevelDecoder>(type: Decoded.Type, decoder: Decoder) -> AnyPublisher<DecodedOutput<Decoded>, HTTPURLError> where Decoder.Input == Data {
+        tryMap { output in
+            do {
+                let decoded = try decoder.decode(type, from: output.data)
+                return (decoded, output.response)
+            } catch {
+                throw HTTPURLError.failDecode(data: output.data, response: output.response, decodeError: error)
+            }
+        }
+        .mapError { $0.asHTTPURLError() }
+        .eraseToAnyPublisher()
+    }
+}
---
+++ HTTPTaskPublisher/Classes/Extensions/URLSession+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  URLSession+Extensions.swift
+//  HTTPTaskPublisher
+//
+//  Created by Nayanda Haberty on 26/5/23.
+//
+
+import Foundation
+
+extension URLSession {
+    
+    public func httpTaskPublisher(for urlRequest: URLRequest) -> HTTPDataTaskPublisher {
+        HTTPDataTaskPublisher(session: self, urlRequest: urlRequest)
+    }
+    
+}

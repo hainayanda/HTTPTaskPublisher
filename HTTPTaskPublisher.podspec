efd9adaff0e912468089969dbba76059b96e2897
Nayanda Haberty <hainayanda@outlook.com>
2023-06-27 09:14:27 +0700

    Bump the version and update documentation

--- HTTPTaskPublisher.podspec
+++ HTTPTaskPublisher.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HTTPTaskPublisher'
-  s.version          = '1.0.0'
+  s.version          = '1.1.0'
   s.summary          = 'Swift Publisher for HTTP request'
 
 # This description is used to generate tags and improve search results.
--- HTTPTaskPublisher/Classes/Base/DataTaskPublisherFactory.swift
+++ HTTPTaskPublisher/Classes/Base/DataTaskPublisherFactory.swift
@@ -13,8 +13,11 @@ protocol DataTaskPublisherFactory {
 }
 
 public enum DuplicationHandling {
+    /// always create a new data task request no matter what
     case alwaysCreateNew
+    /// subscribe to the current ongoing identical task if have any, otherwise, create a new data task
     case useCurrentIfPossible
+    /// cancel the request if there is an ongoing identical task, otherwise, create a new data task
     case dropIfDuplicated
 }
 
